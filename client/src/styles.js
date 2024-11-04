5d1212c3e6d45b1316e98f52bb47ab23149d8b56
hamzaa-bouyahyaa <hamzabouyahya198@gmail.com>
2024-11-04 16:53:39 +0100

    change files

--- client/src/styles.js
+++ client/src/styles.js
@@ -10,7 +10,7 @@ export default makeStyles(() => ({
     alignItems: 'center',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: 'rgb(81, 49, 124)',
   },
   image: {
     marginLeft: '15px',
